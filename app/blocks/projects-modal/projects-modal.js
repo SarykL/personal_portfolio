5d29be97a964b5a3192f8d9f3f47eb2fc289872f
SarykL <saryk.lav@gmail.com>
2016-06-17 10:09:05 +0300

    'my works' section some work is done

---
+++ app/blocks/projects-modal/projects-modal.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import 'magnific-popup';
+
+$(function () {
+	$('.projects-thumbnail__link').magnificPopup({
+		type: 'inline',
+		midClick: true
+	});
+
+
+});
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,8 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import '../blocks/projects-modal/projects-modal';
 
 $(() => {
+
 	svg4everybody();
 });
